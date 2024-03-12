c0e728d07180d7b7347f84c5c5cfdd0b99da5795
SolomDev00 <solomdev0@gmail.com>
2024-03-12 14:08:39 +0200

    Fix error.

--- src/views/website/MarketingSection.tsx
+++ src/views/website/MarketingSection.tsx
@@ -1,5 +1,4 @@
 import { InformationCircleIcon } from "@heroicons/react/20/solid";
-import { motion } from "framer-motion";
 import Button from "../../components/schema/Button";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -22,9 +21,9 @@ const MarketingSection = () => {
       </div>
       <div className="space-y-36 z-30 mb-72">
         <div className="flex flex-col justify-center items-center text-center space-y-12 max-sm:space-y-4">
-          <motion.h3 className="text-5xl text-yhover font-medium max-sm:text-xl">
+          <h3 className="text-5xl text-yhover font-medium max-sm:text-xl">
             {t("home.marketing.title")}
-          </motion.h3>
+          </h3>
           <h5 className="text-xl text-black dark:text-gray-400 w-[550px] text-center max-sm:text-base max-sm:w-[300px]">
             {t("home.marketing.description")}
           </h5>
